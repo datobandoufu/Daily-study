03c25e2004742bd8d50a9458a097b6a3656c6f9c
datobandoufu <w1303049366@163.com>
2023-09-17 17:23:22 +0800

    调整格式

--- event-vue/.eslintrc.js
+++ event-vue/.eslintrc.js
@@ -3,16 +3,17 @@ module.exports = {
 	env: {
 		node: true
 	},
-	extends: [
-		"eslint:recommended",
-		"standard",
-		"plugin:prettier/recommended"
-	],
+	extends: ["plugin:vue/essential", "@vue/standard"],
 	parserOptions: {
 		parser: "@babel/eslint-parser"
 	},
 	rules: {
 		"no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-		"no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
+		"no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+		"no-tabs": 0,
+		indent: ["error", "tab"],
+		quotes: ["error", "double", { avoidEscape: true }],
+		semi: ["error", "always"],
+		"comma-dangle": ["error", "never"]
 	}
 };
